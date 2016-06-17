b2224d90e27e5f73c39c56cc6b178cd862cf71bf
Vitor Maciel <vitormf@yahoo.com>
2016-06-17 00:14:15 -0300

    TopViewController

---
+++ TopViewController/TopViewControllerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TopViewControllerDemo
+//
+//  Created by Vitor Maciel on 6/16/16.
+//  Copyright © 2016 Vitor Maciel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
